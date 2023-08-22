e278052ffb1da5fa1988f1a757617d562e979534
mateob24 <bernalmateoa@gmail.com>
2023-08-21 23:18:26 -0500

    Estructura mas adecuada de las carpetas y archivos

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,7 +1,6 @@
-// import React from 'react'
+
 import { Header } from '../../Header/Header'
 import { Portfolio } from '../../Portfolio/Portfolio'
-// import { Product } from '../../Product/Product.jsx'
 
 
 export const Home = () => {
